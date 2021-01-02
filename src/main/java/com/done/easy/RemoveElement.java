d8703bf00930e599f95f6c21fb44e4f912f5cb06
Ivan Legostin <ivan_legostin@mail.ru>
2021-01-02 20:20:43 +0300

    Remove Element : add problem explanation

--- src/main/java/com/done/easy/RemoveElement.java
+++ src/main/java/com/done/easy/RemoveElement.java
@@ -1,5 +1,16 @@
 package com.done.easy;
 
+/**
+ * Remove Element
+ *
+ * Given an array nums and a value val,
+ * remove all instances of that value in-place
+ * and return the new length.
+ *
+ * Do not allocate extra space for another array,
+ * you must do this by modifying the input array in-place
+ * with O(1) extra memory.
+ */
 public class RemoveElement {
 
     public int removeElement(int[] nums, int val) {

d5d525f2f51b0c3ca1dcddc0a837c9626dfaf9ff
Ivan Legostin <ivan_legostin@mail.ru>
2021-01-09 07:54:21 +0300

    ListNode : equals and hashCode

--- src/main/java/com/done/easy/data/ListNode.java
+++ src/main/java/com/done/easy/data/ListNode.java
@@ -1,5 +1,7 @@
 package com.done.easy.data;
 
+import java.util.Objects;
+
 public class ListNode {
 
     public int val;
@@ -10,4 +12,17 @@ public class ListNode {
     public ListNode(int val) { this.val = val; }
 
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        return val == listNode.val && Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
+    }
 }
